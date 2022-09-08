210e8f62ce47253d5ce6e10762f741b93c76c2d9
Redouane Khlifah <102878054+RedouaneKhlifah@users.noreply.github.com>
2022-09-08 09:22:33 +0100

    Add files via upload

---
+++ Character Input.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[20];
+    printf("what is your name : ");
+    scanf("%s" ,&name);
+    int age;
+    printf("how old are you : ");
+    scanf("%d" ,&age);
+    printf("hello %s you will reach 100 years in %d",name ,2022 - age + 100);
+    return 0 "
+}
+
+
+int age = 44
+float af = 4.5
+char age = 'H'
+
+
